fba28a7ba09d3128c0c35fdff2e949a90605044e
Gabriele Soldati <soldati.gabriele@gmail.com>
2019-05-01 18:45:28 +0100

    commetn

--- routes/web.php
+++ routes/web.php
@@ -5,24 +5,24 @@ $router->get('/', function () use ($router) {
 });
 
 $router->get('/validate', function (\Illuminate\Http\Request $request) {
-    $parole = strtolower('Ei fu Siccome immobile dato mortal sospiro stette spoglia immemore orba tanto spiro cosi percossa attonita terra nunzio sta muta pensando ultima ora uom fatale quando una simile orma pie mortale sua cruenta polvere calpestar Lui folgorante in solio vide il mio genio tacque quando con vece assidua cadde risorse e giacque mille voci al sonito mista non vergin servo encomio codardo oltraggio sorge or commosso al subito sparir tanto raggio scioglie all urna un cantico forse');
-    $parole = explode(' ', $parole);
-    $index = random_int(0, sizeof($parole)-3);
-    $newWord =  $parole[$index] . '_' . $parole[$index+1] . '_' . $parole[$index+2];
+    // $parole = strtolower('Ei fu Siccome immobile dato mortal sospiro stette spoglia immemore orba tanto spiro cosi percossa attonita terra nunzio sta muta pensando ultima ora uom fatale quando una simile orma pie mortale sua cruenta polvere calpestar Lui folgorante in solio vide il mio genio tacque quando con vece assidua cadde risorse e giacque mille voci al sonito mista non vergin servo encomio codardo oltraggio sorge or commosso al subito sparir tanto raggio scioglie all urna un cantico forse');
+    // $parole = explode(' ', $parole);
+    // $index = random_int(0, sizeof($parole)-3);
+    // $newWord =  $parole[$index] . '_' . $parole[$index+1] . '_' . $parole[$index+2];
     
     $word = \App\Word::latest()->firstOrFail();
     $word->increment('attempts');
-    if($word->attempts > 5){
-        // sleep($word->attempts / 6);
-    }
+    // if($word->attempts > 5){
+    //     // sleep($word->attempts / 6);
+    // }
 
     if ($word && $request->password == $word->value) {
-        $word->ip = $request->ip();
-        $word->save();
-        \App\Word::create([
-            'value' => $newWord,
-        ]);
-        return response(['status' => 'ok'], 200);
+        // $word->ip = $request->ip();
+        // $word->save();
+        // \App\Word::create([
+        //     'value' => $newWord,
+        // ]);
+        return response(['status' => 'ok', 'unlock' => 'valid'], 200);
     }
     return response(['status' => 'ko'], 403);
 });
